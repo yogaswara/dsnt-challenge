a092cca0f9e3fcdfc9e8f59c159d51aea24f14f4
gisthayo <gisthayo@gmail.com>
2026-02-28 00:24:39 +0700

    remove validation on echo

--- internal/core/services/echo.go
+++ internal/core/services/echo.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"dsnt-challenge/internal/core/domain"
 	"dsnt-challenge/internal/core/ports"
-	"errors"
 )
 
 type echoService struct{}
@@ -15,9 +14,5 @@ func NewEchoService() ports.EchoService {
 }
 
 func (s *echoService) Echo(ctx context.Context, req domain.EchoRequest) (domain.EchoResponse, error) {
-	if len(req) == 0 {
-		return nil, errors.New("request body cannot be empty")
-	}
-
 	return domain.EchoResponse(req), nil
 }
